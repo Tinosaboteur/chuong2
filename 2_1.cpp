166b13a7e9ce5cc79c1806023af127d4ef0de394
Tinosaboteur <124687056+Tinosaboteur@users.noreply.github.com>
2023-03-03 19:12:18 +0700

    Add files via upload

---
+++ 2_1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int s(int n) {
+    if (n==0)
+    return 1;
+    else
+    return (n * s(n-1));
+}
+
+int main() {
+    int n;
+    printf("nhap n: ");
+    scanf("%d", &n);
+    printf("ket qua: %d",s(n));
+    getch();
+}
